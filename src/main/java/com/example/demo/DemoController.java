ba880cf85b9a82470abefc73516923d54989196a
Ken Chen <chenchaokun@gmail.com>
2022-06-18 22:01:30 +0800

    initial commit

---
+++ src/main/java/com/example/demo/DemoController.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class DemoController {
+
+    @GetMapping("/{name}")
+    public ResponseEntity<String> hello(@PathVariable("name") String name) {
+        return ResponseEntity.ok("Hello " + name);
+    }
+}
